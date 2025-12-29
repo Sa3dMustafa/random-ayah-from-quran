66d54300dfaf5e2d4b148f75989b56e50b5d97a1
Sa3d0o <105570840+Sa3dMustafa@users.noreply.github.com>
2025-12-29 22:30:20 +0200

    add icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
       <head>
+        <link rel="icon" href="@/public/—Pngtree—holy quran flat illustration_6101133 (1).png" type="image/png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
